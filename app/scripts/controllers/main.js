d1c1156e64033ce580ba0afa8569c3cd49529462
millerj1994 <jmm146@duke.edu>
2018-10-26 11:20:23 -0400

    Added more stuff.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,19 @@ angular.module('danimalsApp')
     var danimalsOrientationJSON = 'orientation.json';
   	this.selectedPosition = null;
   	var self = this;
+    this.savedFacts = [];
+    this.showAlert = function(ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      // Modal dialogs should fully cover application
+      // to prevent interaction outside of dialog
+      $mdDialog.show(
+        $mdDialog.alert()
+          .clickOutsideToClose(true)
+          .title('Saved Facts')
+          .textContent(this.savedFacts.join('\n\n\n'))
+          .ok('Got it!')
+      );
+    };
     $http.get(danimalsS3 + danimalsDataJSON).then(function(data){
     	var positions = data.data;
     	self.positions = positions.map(function(position){
@@ -37,6 +50,21 @@ angular.module('danimalsApp')
 
     var specialMessage = null;
 
+    function DialogController($scope, $mdDialog, fact) {
+      $scope.fact = fact;
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+
+      $scope.answer = function(answer) {
+        $mdDialog.hide({status:answer, fact:fact});
+      };
+    }
+
     setInterval(function(){ 
       $http.get(
         danimalsS3 + danimalsSpecialJSON
@@ -46,27 +74,39 @@ angular.module('danimalsApp')
           if (!specialMessage){
             specialMessage = message;
           }
-          console.log(message.fact, specialMessage.fact)
+          //console.log(message.fact, specialMessage.fact)
           if (message.fact!==specialMessage.fact){
+            $mdDialog.show({
+              controller: DialogController,
+              locals: {fact: message.fact},
+              templateUrl: '/views/dialog.html',
+              clickOutsideToClose:false
+            }).then(function(answer) {
+                  if(answer.status){
+                    self.savedFacts.push(answer.fact)
+                  } else {
+                    self.selectedIndex = self.selectedIndex + 5;
+                  }
+                }, function() {
+                  $scope.status = 'You cancelled the dialog.';
+                });
+            /*var confirm = $mdDialog.alert()
+              .title('Here\'s what i found!')
+              .textContent(message.fact)
+              .ok('Read More.')
             $mdDialog.show(
-              $mdDialog.alert()
-                .clickOutsideToClose(true)
-                .title('Here\'s what i found!')
-                .textContent(message.fact)
-                .ariaLabel('Alert Dialog Demo')
-                .ok('Got it!')
-            );
+              confirm
+            );*/
           };
           specialMessage = message;
         }
-      );}, 3000);
+      );}, 2000);
 
     setInterval(function(){ 
       $http.get(
         danimalsS3 + danimalsOrientationJSON
       ).then(
         function(data){
-          console.log(data.data, self.selectedIndex);
           if(data.data.position === 'right'){
             self.selectedIndex = self.selectedIndex + 1;
           } else if (data.data.position === 'left' && self.selectedIndex!==0){
